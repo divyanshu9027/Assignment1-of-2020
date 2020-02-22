3914ffaed865314540b36d24fab55a0f41ae84e9
divyanshu9027 <divyanshu.sharma_iot18@gla.ac.in>
2020-02-22 18:53:50 +0530

    create the case 1 with the appropriate conditions and add the contact to the list and create the object of add field in the main class

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,5 +1,7 @@
 package Main;
 import definition.ContactList;
+import definition.Person;
+
 import java.util.Scanner;
 
 public class Main {
@@ -22,6 +24,33 @@ public class Main {
                     "Press 5 to exit program ");
             int number = scanner.nextInt();
             scanner.nextLine();
+            switch (number) {
+                case 1:
+                    System.out.println("You have chosen to add a new contact: \n" +
+                            "Please enter the name of the Person");
+                    System.out.println("First Name:");
+                    firstName = scanner.nextLine();
+                    System.out.println("Last Name:");
+                    lastName = scanner.nextLine();
+                    contactList.setName(firstName, lastName);
+                    contactList.setFirstNameInList(firstName);
+                    System.out.println("Contact Number:");
+                    contactNumber[0] = scanner.nextLine();
+                    for (int i = 1; i <= 2; i++) {
+                        System.out.println("Would you like to add new Number?");
+                        char response = scanner.next().charAt(0);
+                        if (response == 'y') {
+                            contactNumber[i] = scanner.next();
+                        } else {
+                            break;
+                        }
+                    }
+                    System.out.println("Enter the Email:");
+                    email = scanner.next();
+                    Person person = new Person(firstName, lastName, contactNumber, email);
+                    contactList.add(person);
+                    break;
+
 
         }
-    }}
+    }}}
