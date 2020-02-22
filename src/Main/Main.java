704472ddf04b955eefc17aa112af090a480e6407
divyanshu9027 <divyanshu.sharma_iot18@gla.ac.in>
2020-02-22 18:48:49 +0530

    call the contactlist and create it's object in the main class

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ContactList contactList = new ContactList();
+        String firstName;
+        String lastName;
+        String[] contactNumber = new String[3];
+        String email;
+        boolean flag;
 
     }
 }
