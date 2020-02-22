39a2fd080e43696e581682c115cb6337686d56ff
divyanshu9027 <divyanshu.sharma_iot18@gla.ac.in>
2020-02-22 18:41:48 +0530

    make the string override and use the stringbuffer to make string mutable

--- src/definition/Person.java
+++ src/definition/Person.java
@@ -5,6 +5,20 @@ public class Person {
     private String lastname;
     private String[] contactNo;
     private String email;
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("-------- * -------- * -------- * --------\n");
+        sb.append("First Name: ").append(firstname).append('\n');
+        sb.append("Last Name: ").append(lastname).append('\n');
+        sb.append("Contact No: ");
+        for (int i = 0; i < contactNo.length; i++) {
+            sb.append((contactNo[0] != null && i == 0 ? contactNo[0] : "") + (contactNo[1] != null && i == 1 ? ", " + contactNo[1] : "") +
+                    (contactNo[2] != null && i == 2 ? ", " + contactNo[i] : ""));
+        }
+        sb.append("\nEmail: ").append(email).append('\n');
+        sb.append("-------- * -------- * -------- * --------\n");
+        return sb.toString();
+    }
 
     public Person(String firstname, String lastname, String[] contactNo, String email) {
         this.firstname = firstname;
