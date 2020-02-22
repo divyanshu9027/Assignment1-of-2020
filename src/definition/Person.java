4523deb9863ad01d6b61b5b38df72766414c03c2
divyanshu9027 <divyanshu.sharma_iot18@gla.ac.in>
2020-02-22 18:36:41 +0530

    create constructor for all the fields

--- src/definition/Person.java
+++ src/definition/Person.java
@@ -5,4 +5,11 @@ public class Person {
     private String lastname;
     private String[] contactNo;
     private String email;
+
+    public Person(String firstname, String lastname, String[] contactNo, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.contactNo = contactNo;
+        this.email = email;
+    }
 }
